aa539527db38d73a8791d466aa3a7b06c4d55889
Sumukh Prasad <sumukhprasad@icloud.com>
2024-08-27 16:00:26 +0530

    Change in testcases.

--- 01-hello-developers/testCaseGenerator.py
+++ 01-hello-developers/testCaseGenerator.py
@@ -3,9 +3,9 @@ os.mkdir('./testCases')
 os.mkdir('./testCases/input')
 os.mkdir('./testCases/output')
 
-nouns = ['world', 'people', 'developers']
+nouns = ['world', 'people', 'developers', "dips", "codejam"]*4
 
-for i in range(0, 3):
+for i in range(0, 20):
      n = str(i).zfill(2)
 
      inputFile = open(f'./testCases/input/input{n}.txt', 'a')
